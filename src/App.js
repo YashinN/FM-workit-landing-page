80563c325e657068116ffd22d3945896a51704fd
Yashin <yashinnaran@gmail.com>
2024-02-11 12:40:15 +0200

    Add: purple curved backdrop added to landing page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Layout/Main";
 import HeroSection from "./components/Hero/HeroSection";
+import BackgroundPattern from "./components/Layout/BackgroundPattern";
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
       <Main>
         <HeroSection />
       </Main>
+      <BackgroundPattern />
     </>
   );
 }
